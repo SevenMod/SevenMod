f0cdf04f92d81b593db3fd33b529a29fca0216bf
Steve Guidetti <stevotvr@gmail.com>
2018-11-14 20:11:51 -0800

    Fix namespace

--- Plugin-PlayerCommands/PlayerCommands.cs
+++ Plugin-PlayerCommands/PlayerCommands.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
 
-namespace SevenMod.Plugin.BaseCommands
+namespace SevenMod.Plugin.PlayerCommands
 {
     using SevenMod.Admin;
     using SevenMod.Core;
