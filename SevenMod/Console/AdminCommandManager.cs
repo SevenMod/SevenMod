f26b855d9c308fe2d6f92441397763c1a676a362
Steve Guidetti <stevotvr@gmail.com>
2018-12-09 14:42:06 -0800

    Add admin command overrides

--- SevenMod/Console/AdminCommandManager.cs
+++ SevenMod/Console/AdminCommandManager.cs
@@ -6,6 +6,9 @@
 namespace SevenMod.Console
 {
     using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
     using SevenMod.Admin;
     using SevenMod.Chat;
     using SevenMod.Core;
@@ -16,9 +19,32 @@ namespace SevenMod.Console
     internal class AdminCommandManager
     {
         /// <summary>
+        /// The path to the command overrides configuration file.
+        /// </summary>
+        private static readonly string ConfigPath = $"{SMPath.Config}AdminCommandOverrides.xml";
+
+        /// <summary>
         /// The list of registered admin commands.
         /// </summary>
         private static Dictionary<string, AdminCommand> commands = new Dictionary<string, AdminCommand>();
+
+        /// <summary>
+        /// The map of command access overrides.
+        /// </summary>
+        private static Dictionary<string, AdminFlags> overrides = new Dictionary<string, AdminFlags>();
+
+        /// <summary>
+        /// The watcher for changes to the admin command overrides configuration file.
+        /// </summary>
+        private static FileSystemWatcher overrideWatcher;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="AdminCommandManager"/> class.
+        /// </summary>
+        static AdminCommandManager()
+        {
+            LoadOverrides();
+        }
 
         /// <summary>
         /// Find an existing admin command with the specified name.
@@ -90,7 +116,7 @@ namespace SevenMod.Console
 
             if (senderInfo.RemoteClientInfo != null)
             {
-                var flags = info.AccessFlags;
+                var flags = overrides.ContainsKey(key) ? overrides[key] : info.AccessFlags;
                 if ((flags == 0) || !AdminManager.CheckAccess(senderInfo.RemoteClientInfo, flags))
                 {
                     ChatHelper.ReplyToCommand(senderInfo, "You do not have access to that command");
@@ -108,6 +134,103 @@ namespace SevenMod.Console
         public static void UnloadPlugin(IPlugin plugin)
         {
             commands.RemoveAll((AdminCommand command) => command.Plugin.Equals(plugin));
+        }
+
+        /// <summary>
+        /// Loads the admin command overrides from the config file.
+        /// </summary>
+        private static void LoadOverrides()
+        {
+            overrides.Clear();
+
+            if (!File.Exists(ConfigPath))
+            {
+                CreateOverrideFile();
+            }
+
+            var xml = new XmlDocument();
+            try
+            {
+                xml.Load(ConfigPath);
+            }
+            catch (XmlException e)
+            {
+                SMLog.Error($"Failed reading admin command overrides from {ConfigPath}: {e.Message}");
+                return;
+            }
+
+            foreach (XmlElement element in xml.GetElementsByTagName("Override"))
+            {
+                if (!element.HasAttribute("Command") || !element.HasAttribute("Flag"))
+                {
+                    continue;
+                }
+
+                var command = element.GetAttribute("Command").Trim().ToLower();
+                if (string.IsNullOrEmpty(command))
+                {
+                    continue;
+                }
+
+                var flags = element.GetAttribute("Flag").ToLower().ToCharArray();
+                if (flags.Length == 0)
+                {
+                    overrides[command] = 0;
+                }
+                else if (AdminManager.AdminFlagKeys.TryGetValue(flags[0], out var flag))
+                {
+                    overrides[command] = flag;
+                }
+            }
+
+            if (overrideWatcher == null)
+            {
+                overrideWatcher = new FileSystemWatcher(Path.GetDirectoryName(ConfigPath), Path.GetFileName(ConfigPath));
+                overrideWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+                overrideWatcher.Changed += OnOverrideFileChanged;
+                overrideWatcher.Deleted += OnOverrideFileChanged;
+                overrideWatcher.Renamed += OnOverrideFileChanged;
+                overrideWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        /// <summary>
+        /// Called by the <see cref="overrideWatcher"/> when the admin command overrides configuration file changes.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">A <see cref="FileSystemEventArgs"/> object containing the event data.</param>
+        private static void OnOverrideFileChanged(object sender, FileSystemEventArgs e)
+        {
+            LoadOverrides();
+        }
+
+        /// <summary>
+        /// Creates the admin command overrides configuration file.
+        /// </summary>
+        private static void CreateOverrideFile()
+        {
+            var comment = new StringBuilder("\r\n")
+                .AppendLine("    Use this file to override the default access flag assigned to admin commands.")
+                .AppendLine()
+                .AppendLine("    Override attributes:")
+                .Append("      Command: ").AppendLine("The command to override")
+                .Append("      Flag:    ").AppendLine("The new access flag, or leave empty to remove access restriction")
+                .Append("  ").ToString();
+
+            var settings = new XmlWriterSettings();
+            settings.Indent = true;
+            using (var writer = XmlWriter.Create(ConfigPath, settings))
+            {
+                writer.WriteStartElement("AdminCommandOverrides");
+                writer.WriteComment(comment);
+
+                writer.WriteStartElement("Override");
+                writer.WriteAttributeString("Command", string.Empty);
+                writer.WriteAttributeString("Flag", string.Empty);
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+            }
         }
     }
 }
